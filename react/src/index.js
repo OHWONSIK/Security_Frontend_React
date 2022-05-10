b140ada28f0ca14ef1fb2775e8c1ae8c9e78afa3
Hohartt <sh960530@naver.com>
2022-05-10 21:18:56 +0900

    Add images, basefiles

---
+++ react/src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals"; //performancce 관련된 내용
+import { BrowserRouter } from "react-router-dom";
+
+// id root의 app을 렌더링 시켜준다. ( index.html의 div root 부분)
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
