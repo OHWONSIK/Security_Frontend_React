8bfcd3da65946d1430b6e8b234252e998e64e0c0
Hohartt <sh960530@naver.com>
2022-05-10 21:46:03 +0900

    Update App.js

--- react/src/App.js
+++ react/src/App.js
@@ -10,16 +10,42 @@ import Quicksearchpage from "./page/Quicksearchpage";
 import Customerservicepage from "./page/Customerservicepage";
 import Commonpage from "./page/Commonpage";
 import Incidentnotificationpage from "./page/Incidentnotificationpage";
-
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import React from "react";
 import ReactDOM from "react-dom";
+import Homepage from "./page/homepage";
+import Signuppage from "./page/signuppage";
+import Signupcompletepage from "./page/signupcompletepage";
+import Loginpage from "./page/loginpage";
+import Cardissuancepage from "./page/cardissuancepage";
+import Cardissuancecompletepage from "./page/cardissuancecompletepage";
+import Eventpage from "./page/eventpage";
+import Certificationpage from "./page/certificationpage";
+import Loanapplypage from "./page/loanapplypage";
+import Loancompletepage from "./page/loancompletepage";
+import Transferpage from "./page/transferpage";
+import Transfercompletepage from "./page/transfercompletepage";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Commonpage />} />
+        <Route path="/" element={<Homepage />} />
+        <Route path="/login" element={<Loginpage />} />
+        <Route path="/signup" element={<Signuppage />} />
+        <Route path="/signupcomplete" element={<Signupcompletepage />} />
+        <Route path="/cardissuancepage" element={<Cardissuancepage />} />
+        <Route
+          path="/cardissuancecompletepage"
+          element={<Cardissuancecompletepage />}
+        />
+        <Route path="/event" element={<Eventpage />} />
+        <Route path="/certification" element={<Certificationpage />} />
+        <Route path="/loanapply" element={<Loanapplypage />} />
+        <Route path="/loancomplete" element={<Loancompletepage />} />
+        <Route path="/transfer" element={<Transferpage />} />
+        <Route path="/transfercomplete" element={<Transfercompletepage />} />
+        <Route path="/Commonpage" element={<Commonpage />} />
         <Route
           path="/Bankstatementpage"
           element={<Bankstatementpage />}
