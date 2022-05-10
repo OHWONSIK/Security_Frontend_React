979ca7250550bf81f37f843f305ca1888bc44383
WONSIK <omsoms777@naver.com>
2022-05-10 20:18:49 +0900

    Add cardissuancecomplete.js

---
+++ react/src/components/cardissuancecomplete.js
@@ -0,0 +1,52 @@
+import {Container, Row, Col, Button} from 'react-bootstrap'
+import {Link} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import styles from '../css/cardissuancecomplete.module.css';
+
+
+function Cardissuancecomplete() {
+    return (
+    <div className={styles.Event}>
+        <Container fluid>
+            <Row>
+                <Col sm={2}></Col>
+                    <Col sm={8}>
+                        <img className={styles.checkimage}
+                            src="img/check.png"
+                            alt="checkimage"
+                        />
+                        <h1 className={styles.completemessage}>카드발급이 완료되었습니다.</h1>
+                        <Button className={styles.checkbutton} variant="primary">
+                            <Link to="/">확인</Link>
+                        </Button>{' '}
+                    </Col>
+                <Col sm={2}></Col>
+            </Row>
+        </Container>
+    </div>
+);
+}
+
+export default Cardissuancecomplete;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
