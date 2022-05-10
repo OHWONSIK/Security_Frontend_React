ad7946745acaf7f20c2a3997b32a34c52766faf9
WONSIK <omsoms777@naver.com>
2022-05-10 20:31:48 +0900

    Add loanapplypage.js

---
+++ react/src/page/loanapplypage.js
@@ -0,0 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import TopContent from '../components/TopContent';
+import BottomContent from '../components/BottomContent';
+import Loanapply from '../components/loanapply';
+
+function Loanapplypage() {
+  return (
+    <div className="Loanapplypage">
+      <TopContent />
+      <Loanapply />
+      <BottomContent />
+    </div>
+  );
+}
+
+export default Loanapplypage;
