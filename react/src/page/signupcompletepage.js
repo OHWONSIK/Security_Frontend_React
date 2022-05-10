c49b033c709ea941aa60f3e1de74d13455583b5a
WONSIK <omsoms777@naver.com>
2022-05-10 20:32:34 +0900

    Add signupcompletepage.js

---
+++ react/src/page/signupcompletepage.js
@@ -0,0 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import TopContent from '../components/TopContent';
+import BottomContent from '../components/BottomContent';
+import Signupcomplete from '../components/signupcomplete';
+
+function Signupcompletepage() {
+    return (
+    <div className="Signupcompletepage">
+    <TopContent />
+    <Signupcomplete />
+    <BottomContent />
+    </div>
+);
+}
+
+export default Signupcompletepage;
