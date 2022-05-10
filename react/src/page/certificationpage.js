0c6034790fafe988e5094aafc59958b62d5bdf86
WONSIK <omsoms777@naver.com>
2022-05-10 20:30:59 +0900

    Add certificationpage.js

---
+++ react/src/page/certificationpage.js
@@ -0,0 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import TopContent from '../components/TopContent';
+import BottomContent from '../components/BottomContent';
+import Certification from '../components/certification';
+
+function Certificationpage() {
+  return (
+    <div className="Certificationpage">
+      <TopContent />
+      <Certification />
+      <BottomContent />
+    </div>
+  );
+}
+
+export default Certificationpage;
