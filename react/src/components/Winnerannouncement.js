3ae21e553431b0f8b5426a3ed62d2e55e5c5f0bd
Hohartt <sh960530@naver.com>
2022-05-17 20:44:09 +0900

    Update Winnerannouncement.js

--- react/src/components/Winnerannouncement.js
+++ react/src/components/Winnerannouncement.js
@@ -10,15 +10,21 @@ import styles from "../css/Winnerannouncement.module.css";
 const Winnerannouncement = () => {
   return (
     <div>
-      <Nav fill className={styles.nav} variant="pills" defaultActiveKey="/home">
+      <Nav fill className={styles.nav} variant="pills" defaultActiveKey="/">
         <Nav.Item>
-          <Nav.Link href="/home">진행중인 이벤트</Nav.Link>
+          <Nav.Link href="/Event" active>
+            진행중인 이벤트
+          </Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link href="/home">종료된 이벤트</Nav.Link>
+          <Nav.Link href="/Eventfinishedpage" active>
+            종료된 이벤트
+          </Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link href="/home">당첨자 발표</Nav.Link>
+          <Nav.Link href="/Winnerannouncementpage" active>
+            당첨자 발표
+          </Nav.Link>
         </Nav.Item>
       </Nav>
       <Table striped bordered hover>
