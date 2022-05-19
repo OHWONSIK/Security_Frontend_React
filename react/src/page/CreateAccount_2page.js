9cd573ad85ff2e9e029021b04de6ac788dc1e040
Hohartt <sh960530@naver.com>
2022-05-19 21:16:22 +0900

    Add CreateAccount_2page.js

---
+++ react/src/page/CreateAccount_2page.js
@@ -0,0 +1,25 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import TopContent from "../components/TopContent";
+import BottomContent from "../components/BottomContent";
+import CreateAccount_2 from "../components/CreateAccount_2";
+import { Link } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
+
+const CreateAccount_2page = () => {
+  return (
+    <div>
+      <TopContent />
+      <Container fluid>
+        <Row>
+          <Col>
+            <CreateAccount_2 />
+          </Col>
+        </Row>
+      </Container>
+      <BottomContent />
+    </div>
+  );
+};
+
+export default CreateAccount_2page;
