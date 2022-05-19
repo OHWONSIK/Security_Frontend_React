53071dcb3b3faec077b89e60b3709d77753a242a
WONSIK <omsoms777@naver.com>
2022-05-19 20:51:02 +0900

    Add findpasswordpage.js

---
+++ react/src/page/findpasswordpage.js
@@ -0,0 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link, Route, BrowserRouter as Router } from "react-router-dom";
+import TopContent from '../components/TopContent';
+import BottomContent from '../components/BottomContent';
+import Findpassword from '../components/findpassword';
+
+function Findpasswordpage() {
+  return (
+    <div className="Findpasswordpage">
+      <TopContent />
+      <Findpassword />
+      <BottomContent />
+    </div>
+  );
+}
+
+export default Findpasswordpage;
