aeb6b6610b60f04eeb169be2a2efd776959fe9be
Hohartt <sh960530@naver.com>
2022-05-19 21:16:04 +0900

    Add CreateAccountpage.js

---
+++ react/src/page/CreateAccountpage.js
@@ -0,0 +1,25 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import TopContent from "../components/TopContent";
+import BottomContent from "../components/BottomContent";
+import CreateAccount from "../components/CreateAccount";
+import { Link } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
+
+const CreateAccountpage = () => {
+  return (
+    <div>
+      <TopContent />
+      <Container fluid>
+        <Row>
+          <Col>
+            <CreateAccount />
+          </Col>
+        </Row>
+      </Container>
+      <BottomContent />
+    </div>
+  );
+};
+
+export default CreateAccountpage;
