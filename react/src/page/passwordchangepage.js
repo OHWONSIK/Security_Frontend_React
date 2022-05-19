99460de3b141d4d07cad2b0b4df2e5e5c7dbc135
WONSIK <omsoms777@naver.com>
2022-05-19 20:51:48 +0900

    Add passwordchangepage.js

---
+++ react/src/page/passwordchangepage.js
@@ -0,0 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link, Route, BrowserRouter as Router } from "react-router-dom";
+import TopContent from '../components/TopContent';
+import BottomContent from '../components/BottomContent';
+import Passwordchange from '../components/passwordchange';
+
+function Passwordchangepage() {
+  return (
+    <div className="Passwordchangepage">
+      <TopContent />
+      <Passwordchange />
+      <BottomContent />
+    </div>
+  );
+}
+
+export default Passwordchangepage;
