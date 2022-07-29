d08270cd9766e09dff9ef906f721041466641dc5
오원식 <wonsik@wonsig-ui-MacBookAir.local>
2022-07-29 20:56:29 +0900

    Update loanList.js

--- react/src/components/loanList.js
+++ react/src/components/loanList.js
@@ -9,23 +9,26 @@ import Axios from 'axios';
 
 function LoanList() {
     const [data, setData] = React.useState([]);
-    const [data2, setData2] = React.useState([]);
-
+    // const [data2, setData2] = React.useState([]);
+    let data2 = []
     let i;
 
+    
+
     useEffect(() => {
-        // console.log(data[0])
         for (i = 0; i < data.length; i++) {
             Axios.get('/users/loanlist/' + data[i].loanList
             )
                 .then(res => {
-                    console.log("test")
-                    setData2(res.data.data)
-                    console.log(data2.interestType)
+                    data2.push(res.data.data)
+                    data2 = data2
+                    // data2.push(res.data.data)
+                    // console.log(data2[0].interestType)
+
                 })
                 .catch(err => console.log(err));
         }
-    }, [data]);
+    }, []);
 
 
     useEffect(() => {
@@ -37,7 +40,7 @@ function LoanList() {
                 // console.log(data[0].loanlist)
             })
             .catch(err => console.log(err));
-        
+
         // for (i = 0; i < data.length; i++) {
         //     Axios.get('/users/loanlist/' + data[i].loanlist
         //     )
@@ -47,15 +50,15 @@ function LoanList() {
         //         })
         //         .catch(err => console.log(err));
         // }
-        
-        
+
+
     }, []);
 
 
-    
-    
 
-    
+
+
+
 
     return (
         <div className={styles.LoanList}>
@@ -63,23 +66,23 @@ function LoanList() {
                 <Row>
                     <Col lg={6}>
                         <h4 className={styles.loanList}>대출 목록</h4>
-                        {data.map((data2) => (
-                            <div className = {styles.loanlist}>
-                                <div className={styles.loanType}>{data2.interestType}</div>
+                        {data2.map((data, idx) => (
+                            <div className={styles.loanlist}>
+                                <div className={styles.loanType}>{data[idx].interestType}</div>
                                 <div className={styles.box}></div>
                             </div>
                         ))}
-                        
+
                     </Col>
                     <Col lg={6}>
-                        {data.map((data)=> (
-                            <div className = {styles.loanInfo}>
-                                <div className = {styles.interestRate}>금리: {data.cardNumber}</div>
+                        {data.map((data) => (
+                            <div className={styles.loanInfo}>
+                                <div className={styles.interestRate}>금리: {data2.interestRate}</div>
                                 <div className={styles.amount}>대출금액: {data.amount}원</div>
                             </div>
                         ))}
 
-                        
+
                     </Col>
                     <Button className={styles.checkbutton} variant="primary">
                         <Link to="/myinfo">확인</Link>
