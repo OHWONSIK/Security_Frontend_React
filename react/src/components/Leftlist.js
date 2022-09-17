b035198f70c1e51129a6c0ab8e751c9e807d5b91
오원식 <wonsik@wonsig-ui-MacBookAir.local>
2022-09-17 19:31:49 +0900

    Update Leftlist.js

--- react/src/components/Leftlist.js
+++ react/src/components/Leftlist.js
@@ -9,92 +9,95 @@ const leftList = () => {
     <div>
       <ul className={styles.noBullet}>
         {/* 0번링크는 지금 메인페이지로 해야할 듯?  */}
-        <li className={styles.leftCont1}>
-          <Link to="/product" className={styles.nocolor}>
+        <a href="/product" className={styles.nocolor}>
+        <li className={styles.leftCont1} >
             예적금
-          </Link>
           <div className={styles.divideLineC}></div>
-        </li>
+          </li>
+        </a>
+
+        <a href="/newnews" className={styles.nocolor}>
         <li className={styles.leftCont}>
-          <Link to="/newnews" className={styles.nocolor}>
             새소식
-          </Link>
           <div className={styles.divideLineC}></div>
-        </li>
+          </li>
+        </a>
 
+        <a href="/ongoingevent" className={styles.nocolor}>
         <li className={styles.leftCont}>
-          <Link to="/ongoingevent" className={styles.nocolor}>
             이벤트
-          </Link>
           <div className={styles.divideLineC}></div>
-        </li>
+          </li>
+        </a>
 
+        <a href="/customerservice" className={styles.nocolor}>
         <li className={styles.leftCont}>
-          <Link to="/customerservice" className={styles.nocolor}>
             상담센터
-          </Link>
           <div className={styles.divideLineC}></div>
-        </li>
+          </li>
+        </a>
 
+        <a href="/incidentnotification" className={styles.nocolor}>
         <li className={styles.leftCont}>
-          <Link to="/incidentnotification" className={styles.nocolor}>
             사고신고
-          </Link>
           <div className={styles.divideLineC}></div>
-        </li>
+          </li>
+        </a>
 
+        <a href="/netsec" className={styles.nocolor}>
         <li className={styles.leftCont}>
-          <Link to="/netsec" className={styles.nocolor}>
             보안공지
-          </Link>
           <div className={styles.divideLineC}></div>
-        </li>
+          </li>
+        </a>
 
+        <a href="/" className={styles.nocolor}>
         <li className={styles.leftCont}>
-          <Link to="/" className={styles.nocolor}>
             금융상품
-          </Link>
           <div className={styles.divideLineC}></div>
-        </li>
+          </li>
+        </a>
 
+        <a href="/bankstatement" className={styles.nocolor}>
         <li className={styles.leftCont}>
-          <Link to="/bankstatement" className={styles.nocolor}>
             거래내역
-          </Link>
           <div className={styles.divideLineC}></div>
-        </li>
+          </li>
+        </a>
 
+        <a href="/selectaccount02" className={styles.nocolor}>
         <li className={styles.leftCont}>
-          <Link to="/loanapply" className={styles.nocolor}>
             대출
-          </Link>
           <div className={styles.divideLineC}></div>
-        </li>
+          </li>
+        </a>
 
+        <a href="/quicksearch" className={styles.nocolor}>
         <li className={styles.leftCont}>
-          <Link to="/quicksearch" className={styles.nocolor}>
             빠른거래내역
-          </Link>
           <div className={styles.divideLineC}></div>
-        </li>
+          </li>
+        </a>
 
+        <a href="/transfer" className={styles.nocolor}>
         <li className={styles.leftCont}>
-          <Link to="/transfer" className={styles.nocolor}>
             이체
-          </Link>
           <div className={styles.divideLineC}></div>
-        </li>
+          </li>
+        </a>
 
+        <a href="/cardissuance" className={styles.nocolor}>
         <li className={styles.leftCont}>
-          <Link to="/cardissuance" className={styles.nocolor}>
             카드발급
-          </Link>
           <div className={styles.divideLineC}></div>
-        </li>
+          </li>
+        </a>
 
         <Row>
           <Col>
-            <img src="img/cs_img.jpg" className={styles.cs_size}></img>
+            <a href="/customerservice">
+              <img src="img/cs_img.jpg" className={styles.cs_size}></img>
+            </a>
           </Col>
           <Col>
             <div className={styles.leftCont2}>
