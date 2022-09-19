8a50506850c0d9ee83dd2cc3deadadeb34e82393
오원식 <wonsik@wonsig-ui-MacBookAir.local>
2022-09-19 20:15:18 +0900

    Update loanLists.js

--- react/src/components/loanList.js
+++ react/src/components/loanList.js
@@ -1,4 +1,4 @@
-import { Container, Row, Col, Form, Button } from 'react-bootstrap'
+import { Container, Row, Col, Form, Button, Table } from 'react-bootstrap'
 import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import styles from '../css/loanList.module.css';
@@ -9,27 +9,33 @@ import Axios from 'axios';
 
 function LoanList() {
     const [data, setData] = React.useState([]);
-    // const [interestRate, setInterestRate] = React.useState([]);
-    // const [interestType, setInterestType] = React.useState([]);
-    const [data2, setData2] = React.useState([]);
-    // let data2 = []
-    let i;
 
-    
+    const Tr = ({ info }) => {
+        // let infoReverse = info.slice(0).reverse()
+        return (
+            <tbody>
+                {info.map((item, idx) => {
+                    // console.log(item)
+                    return <Td key={item.id} item={item} />;
+                })}
+            </tbody>
+        );
+    };
 
-    useEffect(() => {
-        for (i = 0; i < data.length; i++) {
-            Axios.get(
-                "/api/v1/guest/loanlist",
-            )
-                .then((res) => {
-                    setData2(res.data.data)
-                })
-                .catch((error) => {
-                    alert(error.response.data.message)
-                });
-        }
-    }, []);
+    const Td = ({ item }) => {
+        return (
+            <>
+                <tr>
+                    <td className={styles.index}>{item.loanList}</td>
+                    <td className={styles.index}>{item.accountNumber}</td>
+                    <td className={styles.index}>{item.amount}</td>
+                    <td className={styles.index}>{item.interestRate}% {item.interestType}</td>
+                    <td className={styles.index}>{item.createDate}</td>
+                    <td className={styles.index}>{item.dueDate}</td>
+                </tr>
+            </>
+        );
+    };
 
 
     useEffect(() => {
@@ -55,28 +61,37 @@ function LoanList() {
         <div className={styles.LoanList}>
             <Container fluid>
                 <Row>
-                    <Col lg={6}>
                         <h4 className={styles.loanList}>대출 목록</h4>
-                        {data.map((data, idx) => (
+                        {/* {data.map((data, idx) => (
                             <div className={styles.loanlist}>
                                 <div className={styles.loanType}>{data.loanList}</div>
-                                <div className={styles.box}></div>
-                            </div>
-                        ))}
-
-                    </Col>
-                    <Col lg={6}>
-                        {data.map((data, idx) => (
-                            <div className={styles.loanInfo}>
                                 <div className={styles.amount}>대출금액: {data.amount}원</div>
-                                <div className={styles.interestRate}>만료날짜: {data.dueDate}</div>
+                                <div className={styles.interest}>금리: {data.interestRate} {data.interestType}</div>
+                                <div className={styles.expireDate}>만료날짜: {data.dueDate}</div>
                             </div>
-                        ))}
+                        ))} */}
+                    <Table striped bordered hover>
+                        <thead>
+                            <tr>
+                                <th>대출상품</th>
+                                <th>대출계좌</th>
+                                <th>대출실행금액</th>
+                                <th>금리</th>
+                                <th>대출일</th>
+                                <th>만료일</th>
+                            </tr>
+                        </thead>
+
+                        <Tr info={data}></Tr>
+                    </Table>
+
+                    
+                        
 
                         
 
 
-                    </Col>
+                    
                 </Row>
             </Container>
         </div>
